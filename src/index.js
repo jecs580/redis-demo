0b6d7259e98cb8974393fca2c8b78bc505ce46b9
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-09-12 21:54:26 -0400

    Guardando en datos de un personaje en redis

--- src/index.js
+++ src/index.js
@@ -2,28 +2,46 @@ const express = require('express');
 const axios = require('axios');
 const responseTime = require('response-time');
 const redis = require('redis');
-
+const {promisify} = require('util')
 const client = redis.createClient({
     host:'localhost',
     port:6379
 })
 
+const GET_ASYNC = promisify(client.get).bind(client)
+const SET_ASYNC = promisify(client.set).bind(client)
+
 const app =  express();
 
 app.use(responseTime())
 app.get('/characters',async(req, res)=>{
-    client.get('characters', async(err,reply)=>{
+
+   try {
+        const reply = await GET_ASYNC('characters')
         if(reply){
             return res.json(JSON.parse(reply))
         }
         const response = await axios.get(`https://rickandmortyapi.com/api/character`);
-        client.set('characters', JSON.stringify(response.data),(err,reply)=>{
-        if(err)console.log(err);
-        console.log(reply);
-        })
+        await SET_ASYNC('characters', JSON.stringify(response.data))
         return res.json(response.data);
-    })
+   } catch (error) {
+       console.log(error);
+   }
     
+})
+// run -p 6379:6379 -d some-redis
+// docker run -p 6379:6379 --name some-redis -d redis
+app.get('/character/:id', async(req, res)=>{
+    try {
+        const reply = await GET_ASYNC(req.params.id);
+        if(reply) return res.json(JSON.parse(reply))
+        const response = await axios.get('https://rickandmortyapi.com/api/character/'+req.params.id)
+        await SET_ASYNC(req.params.id, JSON.stringify(response.data))
+        return res.json(response.data)
+    } catch (error) {
+        return res.status(error.response.status).json({message: error.message})
+        
+    }
 })
 
 app.listen(3000)
